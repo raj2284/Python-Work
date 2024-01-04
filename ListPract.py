3ad4c29874bfc09af02d0f040f9ad0ea99517c75
raj2284 <141821760+raj2284@users.noreply.github.com>
2024-01-04 22:17:50 +0530

    Add files via upload

---
+++ ListPract.py
@@ -0,0 +1,65 @@
+# l= list(range(20))
+# print(l)
+# l[11]="cspit"
+# print(l)
+# print(l[2:15:4])
+# #for reverse order
+# print(l[14:1:-4])
+# #or by negative indexing
+# print(l[-6:-19:-4])
+# l.append(200)
+# n=[300,400]
+# l=l+n
+# print(l)
+# l.insert(6,"charusat")
+# print(l)
+# l.remove("cspit")#removes particular
+# print(l)
+# l.pop()#removes last element
+# del l[3]#removes at particular index
+# print(l)
+# a,b,*c,d,e= l
+#
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+
+# lst = [10,50,40,70,50,30]
+# print(lst.count(50))
+# print(lst.index(70))
+# lst.reverse()
+# print(lst)
+# lst.sort()
+# print(lst)
+# lst.sort(reverse=True)
+# print(lst)
+#
+# #when we dont wanna change the actual list we use sorted !
+#
+# l2 =sorted(lst)
+# print(l2)
+# print(lst)
+
+#Pract 1 B,c
+# List1 =[1,2,3,4,["python","java","C++",[10,20,30]],5,6,7,["apple","banana","orange"]]
+# print(List1[8][2])
+# print(List1[4][0])
+# l2 = List1[::1]
+# print(l2)
+
+
+t =(1,2,3,4,5)
+print(t)
+print(max(t))
+print(min(t))
+print(sum(t))
+
+
+l=[10,20,30,20,10]
+s=set(l)
+l=list(s)
+print(s)
+print(l)
+
